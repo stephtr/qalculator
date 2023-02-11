292ebcdb2772345c773076872dfb58ac07060c12
Stephan Troyer <stephantroyer@live.at>
2023-02-11 02:01:56 +0100

    lib build with O2 optimization

--- lib/wasm-build.sh
+++ lib/wasm-build.sh
@@ -1,2 +1,2 @@
 source ~/emsdk/emsdk_env.sh
-emcc -I ${HOME}/opt/include -L ${HOME}/opt/lib -lqalculate -lgmp -lmpfr -lxml2 --bind calc.cc -o ../website/static/calc.js -O3
+emcc -I ${HOME}/opt/include -L ${HOME}/opt/lib -lqalculate -lgmp -lmpfr -lxml2 --bind calc.cc -o ../website/static/calc.js -O2
