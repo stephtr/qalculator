66231f89293d7a1cd2a464b7e0705a0638d8a3b1
Stephan Troyer <stephantroyer@live.at>
2024-01-11 19:10:22 +0100

    enable ssl for local development

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,6 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), basicSsl()],
 });
