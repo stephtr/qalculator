75b48c9d58960e89e5357912df0305bd668654f0
Stephan Troyer <stephantroyer@live.at>
2024-06-10 11:59:25 +0200

    add caching

--- website/src/routes/api/getCurrencyData/+server.ts
+++ website/src/routes/api/getCurrencyData/+server.ts
@@ -6,15 +6,32 @@ export async function GET(): Promise<Response> {
 		throw new Error('EXCHANGERATE_KEY not set');
 	}
 
+	if (process.env.CACHE) {
+		const cachedEntry = await (process.env.CACHE as any).get(
+			'currencyData',
+		);
+		if (cachedEntry.timestamp > Date.now() - 12 * 3600 * 1000) {
+			return new Response(JSON.stringify(cachedEntry.data), {
+				headers: {
+					'Content-Type': 'application/json',
+					'Access-Control-Allow-Origin': '*',
+					'cache-control': `public, max-age=${6 * 3600}`,
+				},
+			});
+		}
+	}
+
 	const resp = await fetch(
 		`http://api.exchangerate.host/live?source=EUR&access_key=${EXCHANGERATE_KEY}`,
-		// 'force-cache' is not yet supported for Cloudflare Pages
-		{ /* cache: 'force-cache', */ next: { revalidate: 12 * 3600 } },
 	);
 	if (!resp.ok)
 		throw new Error(`error fetching currency data: ${resp.statusText}`);
 
 	const data = await resp.json();
+
+	if (!data.success) {
+		throw new Error(data.error.info as string);
+	}
 
 	const body = {
 		date: new Date(data.timestamp * 1000 - 10 * 24 * 3600 * 1000)
@@ -28,9 +45,16 @@ export async function GET(): Promise<Response> {
 		),
 	} as CurrencyData;
 
+	if (process.env.CACHE) {
+		await (process.env.CACHE as any).put('currencyData', {
+			timestamp: Date.now(),
+			data: body,
+		});
+	}
+
 	return new Response(JSON.stringify(body), {
 		headers: {
-			'Content-Type': 'application/json', // check casing
+			'Content-Type': 'application/json',
 			'Access-Control-Allow-Origin': '*',
 			'cache-control': `public, max-age=${6 * 3600}`,
 		},
