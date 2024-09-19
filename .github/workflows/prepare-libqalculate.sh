005230d92446a19e7e30e9045128645322be3be5
Stephan Troyer <stephantroyer@live.at>
2024-09-20 00:24:43 +0200

    improve staging

--- .github/workflows/prepare-libqalculate.sh
+++ .github/workflows/prepare-libqalculate.sh
@@ -1,34 +1,7 @@
 #!/bin/bash
 
-sudo apt install -y build-essential lzip binutils autoconf intltool libtool automake lbzip2 lzip
 mkdir -p ~/opt/src
 cd ~/opt/src
-
-wget https://ftp.gnu.org/gnu/gmp/gmp-${GMP_VERSION}.tar.lz
-tar xf gmp-${GMP_VERSION}.tar.lz
-cd gmp-${GMP_VERSION}
-emconfigure ./configure --disable-assembly --host none --enable-cxx --prefix=${HOME}/opt # no, the "none" host is not obsolete.
-make
-make install
-cd ..
-
-wget https://ftp.gnu.org/gnu/mpfr/mpfr-${MPFR_VERSION}.tar.xz
-# wget https://www.mpfr.org/mpfr-current/allpatches # if available
-tar xf mpfr-${MPFR_VERSION}.tar.xz
-cd mpfr-${MPFR_VERSION}
-emconfigure ./configure --prefix=${HOME}/opt --with-gmp=${HOME}/opt
-make
-make install
-cd ..
-
-wget http://xmlsoft.org/download/libxml2-${LIBXML2_VERSION}.tar.gz -O libxml2-${LIBXML2_VERSION}.tar.gz
-tar xf libxml2-${LIBXML2_VERSION}.tar.gz 
-cd libxml2-${LIBXML2_VERSION}
-emconfigure ./configure --prefix=${HOME}/opt --disable-shared --without-python
-make
-make install
-ln -s ${HOME}/opt/include/libxml2/libxml ${HOME}/opt/include/libxml
-cd ..
 
 wget https://github.com/Qalculate/libqalculate/archive/refs/tags/v${LIBQALCULATE_VERSION}.tar.gz -O libqalculate-${LIBQALCULATE_VERSION}.tar.gz
 tar xf libqalculate-${LIBQALCULATE_VERSION}.tar.gz
