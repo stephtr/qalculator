95f4a99ba3e4649d5839b3c8835ec83c84620baf
Stephan Troyer <stephantroyer@live.at>
2024-09-29 03:36:15 +0200

    switch to edge runtime

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+export const runtime = 'edge';
+
 export const metadata: Metadata = {
   title: "Qalculator v2",
   description: "Calculating the awesome way",
---
+++ pages/500.tsx
@@ -0,0 +1,3 @@
+export default function Custom500() {
+	return <h1>500 - Server Error</h1>;
+}
