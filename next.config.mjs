524d4eb121b5a1b02948cd75e695974a38b3f069
Stephan Troyer <stephantroyer@live.at>
2024-12-13 22:45:49 +0100

    improve caching

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,21 @@
 import withSerwistInit from "@serwist/next";
 import nextBuildId from "next-build-id";
-import libqalculatePackageJson from "./node_modules/libqalculate-wasm/package.json" assert { type: "json" };
 
 const buildId = await nextBuildId({ dir: import.meta.dirname });
-const libqalculateVersion = libqalculatePackageJson.version;
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    BUILD_ID_ENV: buildId,
-  },
-};
+const nextConfig = {};
 
-const urlsToCache = ["/", "/logo.png", '/api/getCurrencyData'];
+const urlsToCache = ["/", "/api/getCurrencyData"];
 
 const withSerwist = withSerwistInit({
   swSrc: "app/sw.ts",
   swDest: "public/sw.js",
   reloadOnOnline: false,
-  additionalPrecacheEntries: [
-    {
-      url: "/libqalculate.wasm",
-      revision: libqalculateVersion,
-    },
-    ...urlsToCache.map((url) => ({ url, revision: buildId })),
-  ],
+  additionalPrecacheEntries: urlsToCache.map((url) => ({
+    url,
+    revision: buildId,
+  })),
 });
 
 export default withSerwist(nextConfig);
